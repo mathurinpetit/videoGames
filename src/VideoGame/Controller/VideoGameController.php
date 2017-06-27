771fb053fff5a546fc03b612bf0a1da53c31ad34
Mathurin Petit <mathurin.petit@gmail.com>
2017-06-27 07:42:21 +0200

    initialisation

---
+++ src/VideoGame/Controller/VideoGameController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace VideoGame\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class VideoGameController extends Controller
+{
+
+    /**
+     * @Template()
+     */
+    public function indexAction() {
+        return array();
+    }
+}
---
+++ src/VideoGame/VideoGameBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace VideoGame;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VideoGameBundle extends Bundle
+{
+}
