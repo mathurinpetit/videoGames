7a2c7dd9fcdb00776174e1cb12738af7cfbd7a67
Mathurin Petit <mathurin.petit@gmail.com>
2017-11-16 20:28:31 +0100

    maj de l'index

---
+++ web/js/lib/mainlib.js
@@ -0,0 +1,39 @@
+/** prototype pour les numéros de vidéos */
+
+Number.prototype.zeroPad = Number.prototype.zeroPad ||
+     function(base){
+       var nr = this, len = (String(base).length - String(nr).length)+1;
+       return len > 0? new Array(len).join('0')+nr : nr;
+    };
+
+/** prototype pour les string tout remplacer */
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'ig'), replacement);
+};
+
+
+function createCookie(name,value,days,path='') {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + ";" +path;
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
