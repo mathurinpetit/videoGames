bc8e71c252d4a3225e9f557eb9125152f008badc
Mathurin Petit <mathurin.petit@gmail.com>
2017-11-19 16:59:57 +0100

    mis en place du système de cookie

--- web/js/lib/mainlib.js
+++ web/js/lib/mainlib.js
@@ -13,14 +13,14 @@ String.prototype.replaceAll = function(search, replacement) {
 };
 
 
-function createCookie(name,value,days,path='') {
+function createCookie(name,value,days) {
     var expires = "";
     if (days) {
         var date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + value + expires + ";" +path;
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 function readCookie(name) {
