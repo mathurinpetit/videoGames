f6e8869b33e0095c576f67599b188c6bb05a26fd
Mathurin Petit <mathurin.petit@gmail.com>
2018-09-24 12:50:42 +0200

    archive des videos de jacktothesound

--- bin/resize2mobilesize.sh
+++ bin/resize2mobilesize.sh
@@ -4,5 +4,5 @@ cd $1
 mkdir mobile;
 for path in *.mp4 ; do
   name=`echo $path `;
-  ffmpeg -i $name -vf scale="300:-2" -vcodec libx264 -y -an "mobile/"$name
+  ffmpeg -i $name -vf scale="300:-2" -c:v libx264 -preset veryslow -profile:v main -crf 18 -c:a copy "mobile/"$name
 done
