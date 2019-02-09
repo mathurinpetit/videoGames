c21e48d686fa603738066c6f0f04ca076d3f0f78
mathurin petit <mathurin.petit@gmail.com>
2019-02-09 14:52:17 +0100

    script protect name with space

--- bin/mp4towebm.sh
+++ bin/mp4towebm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for path in *.mp4 ; do
-  name=`echo $path | cut -d'.' -f1`;
-  ffmpeg -i $name".mp4" -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis $name".webm"
+  name=`echo "$path" | cut -d'.' -f1`;
+  ffmpeg -i "$name.mp4" -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis "$name.webm"
 done
--- bin/volume-up.sh
+++ bin/volume-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for path in *.mp4 ; do
-  ffmpeg -i $path -vcodec copy -af "volume=10dB" "_bis_"$path ;
-  mv "_bis_"$path $path;
+  ffmpeg -i "$path" -vcodec copy -af "volume=10dB" "_bis_$path" ;
+  mv "_bis_$path" "$path";
 done
